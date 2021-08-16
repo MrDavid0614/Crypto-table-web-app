0524247ef9801e74b64072e1c26108e9f83b7a4e
David <raymondinf23@gmail.com>
2021-08-16 17:53:39 -0400

    Import shared module and crypto table component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
+import { CryptoTableComponent } from './components/crypto-table/crypto-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CryptoTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
